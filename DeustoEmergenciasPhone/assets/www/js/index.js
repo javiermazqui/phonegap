e369d0cd6bbda5d16d530bbc153c9ae86d4f2f3d
jmazquia <jmazquia@MAD-LRBDFEV.usersad.everis.int>
2013-05-19 14:41:32 +0200

    Subida con gmaps

--- DeustoEmergenciasPhone/assets/www/js/index.js
+++ DeustoEmergenciasPhone/assets/www/js/index.js
@@ -1,9 +1,49 @@
 function DOMLoaded(){
 	document.addEventListener("deviceready", phonegapLoaded, false);
+	navigator.geolocation.getCurrentPosition(onSuccess, onError);
 }
 
 function phonegapLoaded(){
-	$(function(){
-		
-	});
+	$('#botonera').append('<img alt="" src="img/emergencias.jpg">');
+}
+
+function onSuccess(position) { 
+    var myLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    map  = new google.maps.Map(document.getElementById('map_canvas'), {
+	mapTypeId: google.maps.MapTypeId.ROADMAP,
+	center: myLocation,
+	zoom: 15
+    });
+    
+    var element = document.getElementById('comentarios');
+    element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                        'Longitude: '          + position.coords.longitude             + '<br />' +
+                        'Altitude: '           + position.coords.altitude              + '<br />' +
+                        'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                        'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+                        'Heading: '            + position.coords.heading               + '<br />' +
+                        'Speed: '              + position.coords.speed                 + '<br />' +
+                        'Timestamp: '          +                                   position.timestamp          + '<br />';
+    var currentPositionMarker = new google.maps.Marker({
+    	position: myLocation,
+    	map: map,
+    	title: "Current position"
+    });
+    var request = { location: myLocation, radius: currentRadiusValue, types: ['policia'] }; 
+    var service = new google.maps.places.PlacesService(map); 
+    service.nearbySearch(request, callback);
+}
+
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+            'message: ' + error.message + '\n');
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+	map: map,
+	position: place.geometry.location
+    }); 
 }
